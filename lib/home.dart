0968ea51c3996f24bbe2031c9197bddb0ae10d48
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-11-03 23:59:29 +0500

    Variations

--- lib/home.dart
+++ lib/home.dart
@@ -23,6 +23,17 @@ class _HomeState extends State<Home> {
                   height: 95,
                   width: 95,
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text("ABC"),
+                      Text("ABC"),
+                      Text("ABC"),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
