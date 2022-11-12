2f82bf5d5d764bc82b124a8350bee44698e0b31d
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-11-12 23:51:39 +0500

    Same for Register page

--- lib/register.dart
+++ lib/register.dart
@@ -10,6 +10,12 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Text(
+          "Register",
+        ),
+      ),
+    );
   }
 }
